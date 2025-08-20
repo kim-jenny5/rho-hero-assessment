2641cbc91bf256c415a794a9813d79ba13621523
Jenny Kim <kimj1905@gmail.com>
2025-08-20 18:56:45 -0400

    adjust grid for all device screens

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,24 +1,41 @@
 import styles from '../../styles/hero.module.css';
 import clsx from 'clsx';
 
-const placementClasses = {
-	// only 3 has been added for demo purpose
-	// but ideally all 9 combinations should be handled
-	'start center': styles.startCenter,
-	'center end': styles.centerEnd,
-	'end start': styles.endStart,
-};
-
 export default function Hero({ src, alt, title, description, placeTitle, placeDescription }) {
 	const sameCell = placeTitle === placeDescription;
+	const [titleRow, titleCol] = placeTitle.split(' ');
+	const [descRow, descCol] = placeDescription.split(' ');
+
+	const rowMap = { start: styles.rowStart, center: styles.rowCenter, end: styles.rowEnd };
+	const colMap = { start: styles.colStart, center: styles.colCenter, end: styles.colEnd };
 
 	return (
 		<>
 			<img src={src} alt={alt} className={styles.bgImg} />
 			<div className={styles.hero}>
-				<section className={clsx(styles.content, sameCell && placementClasses[placeTitle])}>
-					<h1 className={clsx(styles.title, !sameCell && placementClasses[placeTitle])}>{title}</h1>
-					<p className={clsx(styles.description, !sameCell && placementClasses[placeDescription])}>
+				<section
+					className={clsx(
+						styles.content,
+						sameCell ? rowMap[titleRow] : styles.split,
+						sameCell && colMap[titleCol]
+					)}
+				>
+					<h1
+						className={clsx(
+							styles.title,
+							!sameCell && rowMap[titleRow],
+							!sameCell && colMap[titleCol]
+						)}
+					>
+						{title}
+					</h1>
+					<p
+						className={clsx(
+							styles.description,
+							!sameCell && rowMap[descRow],
+							!sameCell && colMap[descCol]
+						)}
+					>
 						{description}
 					</p>
 				</section>
